2f0736d38d8f8233c2615071080bf8603e4a9772
Emilia Trentham <lavernatrentham@gmail.com>
2023-03-09 14:49:29 -0800

    Initial commit

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+// const { gql } = require('apollo-server-express');
+
+const typeDefs = `#graphql
+  type Query {
+   me: User
+}
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User ??
+    removeBook(bookId: String!): User
+   
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
